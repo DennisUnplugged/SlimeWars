00d4f9f81de4534b6ba99c07603a0f1587499710
DennisUnplugged <dennis@tgfloat.com>
2021-06-21 21:09:15 +0300

    Initial Commit!

---
+++ src/main/java/net/unpluggemc/slimewars/Slimewars.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021.
+ * This class was coded by DennisUnplugged. Do not distribute or modify. Do not copy and use for any purpose.
+ */
+
+package net.unpluggemc.slimewars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class Slimewars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
