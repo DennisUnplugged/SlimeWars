b0e4a9c0320c35dfc077039ea3605a97dd5c9845
DennisUnplugged <dennis@tgfloat.com>
2021-06-28 21:17:18 +0300

    Removed a bunch of stuff ;D (Actually everything LoL)

--- src/main/java/net/unpluggemc/slimewars/SlimeWars.java
+++ src/main/java/net/unpluggemc/slimewars/SlimeWars.java
@@ -5,8 +5,6 @@
 
 package net.unpluggemc.slimewars;
 
-import net.unpluggemc.slimewars.manager.GameManager;
-import net.unpluggemc.slimewars.manager.GameState;
 import net.unpluggemc.slimewars.utils.PluginCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
@@ -16,25 +14,22 @@ import org.reflections.Reflections;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 
-public final class Slimewars extends JavaPlugin {
-
-    private GameManager gameManager;
+public final class SlimeWars extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.gameManager = new GameManager(this);
 
         saveDefaultConfig();
 
         registerEvents();
         registerCommands();
 
-        gameManager.changeGameState(GameState.WAITING_FOR_PLAYERS);
+
     }
 
     @Override
     public void onDisable() {
-        gameManager.cleanUp();
+
     }
 
     public void registerEvents() {
@@ -43,7 +38,7 @@ public final class Slimewars extends JavaPlugin {
         for (Class<?> clazz : new Reflections(packageName + ".listeners").getSubTypesOf(Listener.class)) {
             try {
                 Listener l = (Listener) clazz.
-                        getDeclaredConstructor(this.getClass(), gameManager.getClass()).newInstance(this, gameManager);
+                        getDeclaredConstructor(this.getClass()).newInstance(this);
                 Bukkit.getPluginManager().registerEvents(l, this);
             } catch (Exception x) {
                 x.printStackTrace();
@@ -57,7 +52,7 @@ public final class Slimewars extends JavaPlugin {
 
         for (Class<? extends PluginCommand> clazz : new Reflections(packageName + ".commands").getSubTypesOf(PluginCommand.class)) {
             try {
-                PluginCommand pluginCommand = clazz.getDeclaredConstructor(this.getClass(), gameManager.getClass()).newInstance(this, gameManager);
+                PluginCommand pluginCommand = clazz.getDeclaredConstructor(this.getClass()).newInstance(this);
                 Objects.requireNonNull(getCommand(pluginCommand.getCommandInfo().name())).setExecutor(pluginCommand);
             } catch (InstantiationException | InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
                 e.printStackTrace();
