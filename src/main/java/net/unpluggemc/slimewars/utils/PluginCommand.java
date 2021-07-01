bb470272b55aee17e8654ab6cf59df07f214a97d
DennisUnplugged <dennis@tgfloat.com>
2021-07-01 15:54:58 +0300

    Removed a bunch of stuff ;D (Actually everything LoL)

--- src/main/java/net/unpluggemc/slimewars/utils/PluginCommand.java
+++ src/main/java/net/unpluggemc/slimewars/utils/PluginCommand.java
@@ -1,6 +1,23 @@
 /*
- * Copyright (c) 2021.
- * This class was coded by DennisUnplugged. Do not distribute or modify. Do not copy and use for any purpose.
+ * Copyright (c) 2021 DennisUnplugged
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package net.unpluggemc.slimewars.utils;
@@ -15,7 +32,6 @@ import java.util.Objects;
 
 public abstract class PluginCommand implements CommandExecutor {
     private final CommandInfo commandInfo;
-
 
     protected PluginCommand() {
         commandInfo = getClass().getDeclaredAnnotation(CommandInfo.class);
